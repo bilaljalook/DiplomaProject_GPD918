92408ed5c8c18e4e6ef78365102d0df4b5ce2ce8
Unknown <bilaljalook@hotmail.com>
2018-12-28 15:18:20 +0100

    UI mainmenu added.

---
+++ Tanked!/Assets/ButtonControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    public void LoadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("You quikt the game");
+    }
+    
+
+
+}
